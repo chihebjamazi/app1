6e15f63ad97d10d8434f76e16d97266548856ffc
jchih <jchih@DESKTOP-THI3L1V>
2017-07-21 16:04:44 +0200

    added 3rd empty line

--- WebApplication2/Controllers/ValuesController.cs
+++ WebApplication2/Controllers/ValuesController.cs
@@ -13,12 +13,18 @@ namespace WebApplication2.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
+
             return new string[] { "value1", "value2" };
+
+
 
         }
 
         // GET api/values/5
         public string Get(int id)
+
+
+
         {
             return "value";
         }
